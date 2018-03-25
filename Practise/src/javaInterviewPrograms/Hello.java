2d677cd8cd02e55d08dc64c55b44a3a2554c6d8c
Anand <Anand@192.168.1.10>
2018-03-26 01:15:51 +0530

    ok

--- Practise/src/javaInterviewPrograms/Hello.java
+++ Practise/src/javaInterviewPrograms/Hello.java
@@ -1,15 +1,16 @@
 package javaInterviewPrograms;
 
-public class Hello {
+public class Hello   {
 
 	public static void main(String[] args) {
 	
 		
 		for(int i=0;i<10;i++)
 		{
-			System.out.println("Hello World  "+i);
+			System.out.println("Hello  World  "+i);
 		}
 
 	}
 
 }
+    
